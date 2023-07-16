479aa2a8ddb032a059a5c2ebccf864c69203d962
Aldo Emilio Juarez Hernandez <62711991+Aldo8738@users.noreply.github.com>
2023-07-16 17:04:33 +0200

    ...

--- src/sensors.py
+++ src/sensors.py
@@ -1,7 +1,10 @@
-import time
+from flask import Flask, jsonify
 import seeed_dht
 import smbus
 from grove.display.jhd1802 import JHD1802
+import threading
+
+app = Flask(__name__)
 
 DEVICE = 0x23  # Default device I2C address
 
@@ -9,71 +12,96 @@ POWER_DOWN = 0x00  # No active state
 POWER_ON = 0x01  # Power on
 RESET = 0x07  # Reset data register value
 
-# Start measurement at 1lx resolution. Time typically 120ms
-# Device is automatically set to Power Down after measurement.
 ONE_TIME_HIGH_RES_MODE_1 = 0x20
 
-# bus = smbus.SMBus(0) # Rev 1 Pi uses 0
-bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
+bus = smbus.SMBus(1)  # Rev 2 Pi uses I2C bus 1
 
 def convertToNumber(data):
-    # Simple function to convert 2 bytes of data
-    # into a decimal number. Optional parameter 'decimals'
-    # will round to specified number of decimal places.
     result = (data[1] + (256 * data[0])) / 1.2
     return result
 
 def readLight(addr=DEVICE):
-    # Read data from I2C interface
     data = bus.read_i2c_block_data(addr, ONE_TIME_HIGH_RES_MODE_1)
     return convertToNumber(data)
 
-def main():
-    # Grove - 16x2 LCD RGB Backlight connected to I2C port
-    lcd = JHD1802()
-
-    lcd.setCursor(0, 0)
-    lcd.write('Herby!')
-
-    # Temperature
+def read_temperature():
     sensor = seeed_dht.DHT("11", 12)
-    
-    temperature_check_interval = 10 * 60  # 10 minutes
-    light_check_interval = 30 * 60  # 30 minutes
-    
-    next_temperature_check = time.time()
-    next_light_check = time.time()
+    humidity, temperature = sensor.read()
+    if humidity is not None:
+        return {
+            'humidity': humidity,
+            'temperature': temperature
+        }
+    else:
+        return {
+            'temperature': temperature
+        }
 
+def read_light():
+    light_level = readLight()
+    return {
+        'light_level': light_level
+    }
+
+def update_sensor_values():
     while True:
-        current_time = time.time()
+        temperature_data = read_temperature()
+        light_data = read_light()
 
-        if current_time >= next_temperature_check:
-            humi, temp = sensor.read()
-            if humi is not None:
-                print('DHT{0}, humidity {1:.1f}%, temperature {2:.1f}*'.format(sensor.dht_type, humi, temp))
-            else:
-                print('DHT{0}, humidity & temperature: {1}'.format(sensor.dht_type, temp))
-                
-            if temp > 30:
-                lcd.setCursor(1, 0)
-                lcd.write('Temp > 30C')
-            elif temp < 10:
-                lcd.setCursor(1, 0)
-                lcd.write('Temp < 10C')
-            else:
-                lcd.setCursor(1, 0)
-                lcd.write('             ')  # Clear the line
-            
-            next_temperature_check = current_time + temperature_check_interval
+        # Update the global variables holding the sensor values
+        global temperature_value
+        global light_level_value
+        temperature_value = temperature_data
+        light_level_value = light_data
 
-        if current_time >= next_light_check:
-            lightLevel = readLight()
-            print("Light Level: " + format(lightLevel, '.2f') + " lx")
-            next_light_check = current_time + light_check_interval
+        # Display the sensor values on the LCD screen
+        display_temperature(temperature_data)
+        display_light_level(light_data)
 
-        time.sleep(1)
-        # lcd.write(format(lightLevel, '.2f'))
+        # Sleep for the specified interval
+        time.sleep(temperature_check_interval)
 
+def display_temperature(temperature_data):
+    lcd.setCursor(1, 0)
+    if 'temperature' in temperature_data:
+        temperature = temperature_data['temperature']
+        if temperature > 30:
+            lcd.write('Temp > 30C   ')
+        elif temperature < 10:
+            lcd.write('Temp < 10C   ')
+        else:
+            lcd.write('              ')
+
+def display_light_level(light_data):
+    lcd.setCursor(0, 0)
+    if 'light_level' in light_data:
+        light_level = light_data['light_level']
+        lcd.write('Light Level: {:.2f} lx'.format(light_level))
+    else:
+        lcd.write('              ')
+
+# Grove - 16x2 LCD RGB Backlight connected to I2C port
+lcd = JHD1802()
+lcd.setCursor(0, 0)
+lcd.write('Herby!')
+
+# Global variables to hold the sensor values
+temperature_value = {}
+light_level_value = {}
+
+temperature_check_interval = 10 * 60  # 10 minutes
+
+# Start a separate thread to update the sensor values
+update_thread = threading.Thread(target=update_sensor_values)
+update_thread.start()
+
+@app.route('/temperature')
+def temperature():
+    return jsonify(temperature_value)
+
+@app.route('/light')
+def light():
+    return jsonify(light_level_value)
 
 if __name__ == '__main__':
-    main()
+    app.run()
