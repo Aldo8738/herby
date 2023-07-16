78c514c307f930508918d8164fa14ba961888fc9
Aldo Emilio Juarez Hernandez <62711991+Aldo8738@users.noreply.github.com>
2023-07-16 18:12:30 +0200

    app

---
+++ app/script.js
@@ -0,0 +1,14 @@
+// Fetch data from Flask API
+fetch('/temperature')
+    .then(response => response.json())
+    .then(data => {
+        const temperatureElement = document.getElementById('temperature');
+        temperatureElement.textContent = `Temperature: ${data.temperature}°C`;
+    });
+
+fetch('/humidity')
+    .then(response => response.json())
+    .then(data => {
+        const humidityElement = document.getElementById('humidity');
+        humidityElement.textContent = `Humidity: ${data.humidity}%`;
+    });
